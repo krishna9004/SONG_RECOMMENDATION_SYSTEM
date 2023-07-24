b18399febd004363c399884738368547742e8694
KRISHNA <91628566+krishna9004@users.noreply.github.com>
2023-07-24 11:56:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import pickle
 import streamlit as st
-import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 # laoding models
